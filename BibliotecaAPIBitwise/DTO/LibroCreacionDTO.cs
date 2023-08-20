a901bfd0d386153c25321dab48a1bfa05ede788e
Leandro Silva <leandro.jj.silva@gmail.com>
2023-08-20 12:37:36 -0300

    Clase 5 de academia Bitwise

--- BibliotecaAPIBitwise/DTO/LibroCreacionDTO.cs
+++ BibliotecaAPIBitwise/DTO/LibroCreacionDTO.cs
@@ -1,6 +1,13 @@
-﻿namespace BibliotecaAPIBitwise.DTO
+﻿using BibliotecaAPIBitwise.Models;
+
+namespace BibliotecaAPIBitwise.DTO
 {
     public class LibroCreacionDTO
     {
+        public string Titulo { get; set; } = null!;
+        public bool ParaPrestar { get; set; }
+        public string FechaLanzamiento { get; set; }
+        public int AutorId { get; set; }
+        public int GeneroId { get; set; }
     }
 }
--- BibliotecaAPIBitwise/DTO/LibroDTO.cs
+++ BibliotecaAPIBitwise/DTO/LibroDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace BibliotecaAPIBitwise.DTO
+﻿using BibliotecaAPIBitwise.Models;
+
+namespace BibliotecaAPIBitwise.DTO
 {
     public class LibroDTO
     {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = null!;
+        public bool ParaPrestar { get; set; }
+        public DateTime FechaLanzamiento { get; set; }
+        public string NombreAutor { get; set; }
+        public string NombreGenero { get; set; }
+        public HashSet<Comentario> Comentarios { get; set; } = new HashSet<Comentario>();
     }
 }
